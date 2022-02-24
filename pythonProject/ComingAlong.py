c99c8cc42682c362860b2482256bef3219de243d
Dixon <dixonl2004@gmail.com>
2022-02-24 15:01:56 -0500

    please work

--- pythonProject/ComingAlong.py
+++ pythonProject/ComingAlong.py
@@ -2,6 +2,7 @@ import os
 import random
 SENTINEL = 0
 
+
 # function clears the console for cleaner use of the program
 def clear():
     os.system('cls')
@@ -92,137 +93,70 @@ def check_stats():
             checked_character = input("Type 0 to return to previous screen ")
 
 
-# This function takes the input of how many stat points the enemy has and the level of the enemy
-# and runs equations to assign values to each stat of the opponent within the range of given points
-# with a random chance to either increase the value or decrease the value of the stat within a range
-# based off of a proportion in respect to the ratio of points to stats.
-def enemy_point_assign(stat_points, enemy_level):
-    base_points = stat_points/5
-    enemy_level = enemy_level
-    enemy_strength = 0
-    enemy_mag = 0
-    enemy_defe = 0
-    enemy_health = 0
-    enemy_luck = 0
-    enemy_list = []
-    for i in range(5):
-        point_add_check = random.randint(0, 1)
-        if i == 0:
-            if point_add_check == 0:
-                enemy_strength = base_points - random.randint(0, int(base_points))
-                if enemy_strength == 0:
-                    enemy_strength = 1
-                stat_points = stat_points - enemy_strength
-
-                if stat_points <= 0:
-                    enemy_defe = 1
-                    enemy_health = 1
-                    enemy_luck = 1
-                    enemy_mag = 1
-                    break
-
-            elif point_add_check == 1:
-                enemy_strength = base_points + random.randint(0, int(base_points))
-                stat_points = stat_points - enemy_strength
-                if stat_points <= 0:
-                    enemy_mag = 1
-                    enemy_defe = 1
-                    enemy_health = 1
-                    enemy_luck = 1
-                    break
-
-        elif i == 1:
-            if point_add_check == 0:
-                enemy_mag = base_points - random.randint(0, int(base_points))
-                if enemy_mag == 0:
-                    enemy_mag = 1
-                stat_points = stat_points - enemy_mag
-                if stat_points <= 0:
-                    enemy_defe = 1
-                    enemy_health = 1
-                    enemy_luck = 1
-                    break
-            elif point_add_check == 1:
-                enemy_mag = base_points + random.randint(0, int(base_points))
-                stat_points = stat_points - enemy_mag
-                if stat_points <= 0:
-                    enemy_defe = 1
-                    enemy_health = 1
-                    enemy_luck = 1
-                    break
-
-        elif i == 2:
-            if point_add_check == 0:
-                enemy_defe = base_points - random.randint(0, (int(base_points)))
-                if enemy_defe == 0:
-                    enemy_defe = 1
-                stat_points = stat_points - enemy_defe
-                if stat_points <= 0:
-                    enemy_health = 1
-                    enemy_luck = 1
-                    break
-            elif point_add_check == 1:
-                enemy_defe = base_points + random.randint(0, (int(base_points)))
-                stat_points = stat_points - enemy_defe
-                if stat_points <= 0:
-                    enemy_health = 1
-                    enemy_luck = 1
-                    break
-
-        elif i == 3:
-            if point_add_check == 0:
-                enemy_health = base_points - random.randint(0, int(base_points))
-                if enemy_health == 0:
-                    enemy_health = 1
-                stat_points = stat_points - enemy_health
-                if stat_points <= 0:
-                    enemy_luck = 1
-                    break
-            elif point_add_check == 1:
-                enemy_health = base_points + random.randint(0, int(base_points))
-                stat_points = stat_points - enemy_health
-                if stat_points <= 0:
-                    enemy_luck = 1
-                    break
-        elif i == 4:
-            enemy_luck = stat_points
-
-    new_enemy = BasicEnemy(enemy_strength, enemy_mag, enemy_defe, enemy_health, enemy_luck,"" , enemy_level)
-    enemy_list.append(new_enemy)
-
-    return vars(enemy_list[0])
+def enemy_stat_set(enemy, phys, mag, defe, health, luck):
+    enemy.phys = phys
+    enemy.mag = mag
+    enemy.defe = defe
+    enemy.health = health
+    enemy.luck = luck
 
 
 # The function will generate a random amount of enemies from 1 to 4 and will then assign them a level
 # and then run the function of enemy_point_assign() which will give each enemy random statistics
 # within a set range based on the level of the enemy
-def enemies_present():
-    amount_enemies = random.randint(1, 4)
-    print("There are " + str(amount_enemies) + " enemies present. ")
+def enemies_present(character):
     enemy_list = []
-    enemy_name_list = ["e1", "e2", "e3", "e4"]
-    enemy_position = 0
-    for i in range(amount_enemies):
-        enemy_level = player_main.level
-        level_add_check = random.randint(0,1)
-        if level_add_check == 0:
-            if player_main.level > 1:
-                enemy_level = enemy_level - 1
-        elif level_add_check == 1:
-            enemy_level = enemy_level + 1
-        stat_points = 25 + (5*(enemy_level - 1))
-        new_enemy = enemy_point_assign(stat_points, enemy_level)
-        enemy_list.append(new_enemy)
+    num_enemies = random.randint(1, 4)
+    for i in range(num_enemies):
+        enemy_phys = 0
+        enemy_mag = 0
+        enemy_defense = 0
+        enemy_health = 0
+        enemy_luck = 0
+        enemy_level = int(character.level) + random.randint(0,2)
+        enemy_points = 25 + enemy_level * 5
+        while enemy_points > 0:
+            print("Enemy " + str(i))
+            enemy_phys = random.randint(1, enemy_points//2)
+            enemy_points -= enemy_phys
+            if enemy_points <= 0:
+                break
+            enemy_mag = random.randint(1, enemy_points//2)
+            enemy_points -= enemy_mag
+            if enemy_points <= 0:
+                break
+            enemy_defense = random.randint(1, enemy_points//2)
+            enemy_points -= enemy_defense
+            if enemy_points <= 0:
+                break
+            enemy_health = random.randint(1, enemy_points//2)
+            enemy_points -= enemy_health
+            if enemy_points <= 0:
+                break
+            enemy_luck = enemy_points
+            enemy_points -= enemy_luck
+        if enemy_mag == 0:
+            enemy_mag = 1
+        if enemy_defense == 0:
+            enemy_defense = 1
+        if enemy_health == 0:
+            enemy_health = 1
+        if enemy_luck == 0:
+            enemy_luck = 1
+        if i == 0:
+            enemy_stat_set(enemy_one, enemy_phys, enemy_mag, enemy_defense, 100, enemy_luck)
+            enemy_list.append(enemy_one)
+        elif i == 1:
+            enemy_stat_set(enemy_two, enemy_phys, enemy_mag, enemy_defense, enemy_health, enemy_luck)
+            enemy_list.append(enemy_two)
+        elif i == 2:
+            enemy_stat_set(enemy_three, enemy_phys, enemy_mag, enemy_defense, enemy_health, enemy_luck)
+            enemy_list.append(enemy_three)
+        elif i == 3:
+            enemy_stat_set(enemy_four, enemy_phys, enemy_mag, enemy_defense, enemy_health, enemy_luck)
+            enemy_list.append(enemy_four)
+
     return enemy_list
-
-
-    """
-        if i <= len(enemy_list):4
-        
-            print((enemy_list[i]))
-        enemy_position = enemy_position + 1
-    print(enemy_list)
-    """
 
 
 def enemy_stats(enemies, enemy_list):
@@ -241,104 +175,15 @@ def specific_enemy_receiving_stats(enemy_number, enemy_list):
     return current_enemy_stats_list
 
 
-def basic_attack(enemy_stats, character):
-    enemy_health = enemy_stats[0]
-    enemy_defense = enemy_stats[1]
-    character_damage = character.phys
-    modified_character_damage = (character_damage - random.randint(0, int(enemy_defense)))
-    if modified_character_damage < 0:
-        modified_character_damage = 0
-    enemy_final_health = enemy_health - modified_character_damage
-    return enemy_final_health
+def basic_attack(character, enemy):
+    enemy_health = enemy.health
+    player_damage = character.phys
+    enemy_new_health = enemy_health - player_damage
+    return enemy_new_health
 
 
 # The function will say which characters turn it is and prompt them with the possible actions they
 # can take which each then router to separate functions based on the user of the input
-def player_turn(character):
-    clear()
-    print("It is " + character.name + "s turn")
-    analyzed_list = enemies_present()
-    display_list = list(analyzed_list)
-
-    print("1: Normal Attack, 2: Skill, 3: Defend, 4: Item, 5: Analyze")
-    player_choice = int(input("What action would you like to take? "))
-    if player_choice == 1:
-        enemy_stats(len(analyzed_list), analyzed_list)
-        """
-        enemy_health = enemy_stats['health']
-        print(enemy_health)
-        new_enemy_health = enemy_health - 5
-        enemy_stats['health'] = new_enemy_health
-        print(enemy_stats['health'])
-        print(enemy_stats)
-        for i in range(len(analyzed_list)):
-            pass
-        print(analyzed_list1)
-        """
-        pick_enemy = int(input("Which enemy do you want to attack "))
-        current_enemy_stats = specific_enemy_receiving_stats(pick_enemy - 1, analyzed_list)
-        new_enemy_health = basic_attack(current_enemy_stats, character)
-        damage_dealt = current_enemy_stats[0] - new_enemy_health
-        if new_enemy_health > 0:
-            print(character.name + " dealt " + str(damage_dealt) + " to enemy " + str(pick_enemy))
-            print("That enemy is now at " + str(new_enemy_health) + " health.")
-            current_enemy_stats = analyzed_list[pick_enemy - 1]
-            current_enemy_stats['health'] = new_enemy_health
-            for i in range(len(analyzed_list)):
-                print(analyzed_list[i])
-        else:
-            print(character.name + " dealt a killing blow to enemy " + str(pick_enemy))
-            del analyzed_list[pick_enemy - 1]
-            remaining_enemies = len(analyzed_list) - 1
-            print("There are now " + str(remaining_enemies) + "enemies remaining")
-
-    if player_choice == 2:
-        clear()
-        for i in range(len(character.skill)):
-            skill = character.skill[i]
-            print(str(i+1) + ": " + str(skill.name))
-        skill_choice = int(input("Choose what skill to use: "))
-        used_skill = character.skill[skill_choice-1]
-        clear()
-        enemy_stats(len(analyzed_list), analyzed_list)
-        """
-        enemy_health = enemy_stats['health']
-        print(enemy_health)
-        new_enemy_health = enemy_health - 5
-        enemy_stats['health'] = new_enemy_health
-        print(enemy_stats['health'])
-        print(enemy_stats)
-        for i in range(len(analyzed_list)):
-            pass
-        print(analyzed_list1)
-        """
-        pick_enemy = int(input("Which enemy do you want to attack "))
-        current_enemy_stats = specific_enemy_receiving_stats(pick_enemy - 1, analyzed_list)
-        print(current_enemy_stats)
-        new_enemy_health = basic_attack(current_enemy_stats, character)
-        print(new_enemy_health)
-        damage_dealt = current_enemy_stats[0] - new_enemy_health
-        print(damage_dealt)
-        """
-        if new_enemy_health > 0:
-            print(character.name + " dealt " + str(damage_dealt) + " to enemy " + str(pick_enemy))
-            print("That enemy is now at " + str(new_enemy_health) + " health.")
-            current_enemy_stats = analyzed_list[pick_enemy - 1]
-            current_enemy_stats['health'] = new_enemy_health
-            for i in range(len(analyzed_list)):
-                print(analyzed_list[i])
-        else:
-            print(character.name + " dealt a killing blow to enemy " + str(pick_enemy))
-            del analyzed_list[pick_enemy - 1]
-            remaining_enemies = len(analyzed_list) - 1
-            print("There are now " + str(remaining_enemies) + "enemies remaining")
-        """
-    if player_choice == 5:
-        print(character.name + " analyzed the enemies and determined their stats. ")
-        for i in range(len(display_list)):
-            print(display_list[i])
-
-
 def skill_type_check(skill, character):
     if skill.type == "phys":
         character_stat = character.phys
@@ -358,24 +203,105 @@ def character_luck_range(character):
         return 0
 
 
-def skill_damage(skill, character):
+def skill_damage(skill, character, enemy):
+    total_damage_output = 0
     character_stat = skill_type_check(skill, character)
     for i in range(skill.hits):
         hit_check = character_luck_range(character)
         if hit_check == 0:
             print("Missed! ")
-            return 0
+            total_damage_output = total_damage_output + 0
         elif hit_check == 1:
-            damage_output = skill.modifier * character_stat
-            print(str(character.name) + " used " + str(skill.name) + "!" + " It did " + str(damage_output) + " damage.")
-            return damage_output
+            damage_output = (skill.modifier * character_stat)
+            print(damage_output)
+            total_damage_output = total_damage_output + damage_output
+            print(enemy.name + " now has " + str(enemy.health - total_damage_output))
 
+    final_damage_output = enemy.health - total_damage_output
+    return final_damage_output
+
+
+def show_enemy_stats(enemy_list):
+    for i in range(len(enemy_list)):
+        print(enemy_list[i].name + "'s health is: " + str(enemy_list[i].health))
+        print(enemy_list[i].name + "'s physical attack is: " + str(enemy_list[i].phys))
+        print(enemy_list[i].name + "'s magical attack is: " + str(enemy_list[i].mag))
+        print(enemy_list[i].name + "'s defense is: " + str(enemy_list[i].defe))
+        print(enemy_list[i].name + "'s luck is: " + str(enemy_list[i].luck))
 
 
 def character_skill_learn(skill, character):
     print(character.name + " learned the skill " + skill.name)
     character.skill.append(skill)
 
+
+def player_turn(character, list_enemies):
+    clear()
+    while len(list_enemies) > 0:
+        print("There are " + str(len(list_enemies)) + " enemies present.")
+        print("It is " + character.name + "'s turn.")
+        print("1: Basic Attack")
+        print("2: Skills")
+        action_choice = int(input("Choose which actions to take: "))
+        clear()
+        if action_choice == 1:
+            clear()
+            for i in range(len(list_enemies)):
+                print(str(i+1) + ": " + list_enemies[i].name + " has " + str(list_enemies[i].health) + " health")
+            enemy_selection = int(input("Which enemy do you wish to attack? "))
+            enemy_choice = list_enemies[enemy_selection - 1]
+            print(character.name + " attacked " + enemy_choice.name)
+            new_enemy_health = basic_attack(character, enemy_choice)
+            enemy_choice.health = new_enemy_health
+            clear()
+            if enemy_choice.health > 0:
+                print(character.name + " hit the enemy with a basic attack.")
+                print(enemy_choice.name + " now has " + str(enemy_choice.health) + " health.")
+            else:
+                print(enemy_choice.name + " has perished")
+                del list_enemies[enemy_selection - 1]
+
+        if action_choice == 2:
+            clear()
+            for i in range(len(character.skill)):
+                print(str(i + 1) + ": " + character.skill[i].name)
+            skill_choice = int(input("Which skill do you wish to use? "))
+            clear()
+            skill_choice = character.skill[skill_choice - 1]
+            if skill_choice.enemies == 0:
+                for i in range(len(list_enemies)):
+                    print(str(i+1) + ": " + list_enemies[i].name + " has " + str(list_enemies[i].health) + " health")
+                enemy_selection = int(input("Which enemy do you wish to attack? "))
+
+                enemy_choice = list_enemies[enemy_selection - 1]
+                start_enemy_health = enemy_choice.health
+                new_enemy_health = skill_damage(skill_choice, character, enemy_choice)
+                enemy_choice.health = new_enemy_health
+                if enemy_choice.health > 0:
+                    if new_enemy_health != start_enemy_health:
+                        print(skill_choice.name + " hit " + enemy_choice.name )
+                        print(enemy_choice.name + " now has " + str(enemy_choice.health) + " health.")
+                    else:
+                        print(skill_choice.name + " missed.")
+                else:
+                    print(skill_choice.name + " dealt a killing blow. ")
+                    print(enemy_choice.name + " has perished")
+                    del list_enemies[enemy_selection - 1]
+            else:
+                for i in range(len(list_enemies)):
+                    start_enemy_health = list_enemies[i].health
+                    new_enemy_health = skill_damage(skill_choice, character, list_enemies[i])
+                    list_enemies[i].health = new_enemy_health
+                    if list_enemies[i].health > 0:
+                        if new_enemy_health != start_enemy_health:
+                            print(skill_choice.name + " hit " + list_enemies[i].name)
+                            print(list_enemies[i].name + " now has " + str(list_enemies[i].health) + " health.")
+                        else:
+                            print(skill_choice.name + " missed.")
+                    else:
+                        print(skill_choice.name + " dealt a killing blow. ")
+                        print(list_enemies[i].name + " has perished")
+                        del list_enemies[i]
 
 class MainCharacter:
 
@@ -402,7 +328,7 @@ class BasicEnemy:
 
 
 class Skill:
-    def __init__(skill, type, level, enemies, hits, name):
+    def __init__(skill, type, level, hits, enemies, name):
         skill.type = type
         skill.level = level
         skill.enemies = enemies + 1
@@ -421,12 +347,17 @@ class Skill:
 
 P000 = Skill("phys", 0, 0, 0, "Bash")
 P100 = Skill("phys", 1, 0, 0, "Bonk")
+P073 = Skill("phys", 0, 8, 3, "Hassou Tobi")
 M000 = Skill("mag", 0, 0, 0, "Fireball")
-player_main = MainCharacter(5, 5, 5, 5, 5, "Bill", 1, [])
+player_main = MainCharacter(5, 5, 5, 5, 5, "Bill", 1, [P000, P100, P073])
 brawler = MainCharacter(8, 3, 5, 8, 1, "Jeff", 1, [])
-mage = MainCharacter(3, 8, 3, 5, 6, "Richard", 1, [])
+mage = MainCharacter(3, 8, 3, 5, 6, "Richard", 1, [M000])
 tank = MainCharacter(2, 2, 8, 8, 5, "Bruce", 1, [])
+enemy_one = BasicEnemy(0, 0, 0, 0, 0, "Enemy One", 0)
+enemy_two = BasicEnemy(0, 0, 0, 0, 0, "Enemy Two", 0)
+enemy_three = BasicEnemy(0, 0, 0, 0, 0, "Enemy Three", 0)
+enemy_four = BasicEnemy(0, 0, 0, 0, 0, "Enemy Four", 0)
 
-character_skill_learn(P000, player_main)
-character_skill_learn(P100, player_main)
-player_turn(player_main)
+
+enemy_list = enemies_present(player_main)
+player_turn(player_main, enemy_list)
