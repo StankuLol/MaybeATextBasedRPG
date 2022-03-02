587f6fb3e7032c4eb353f77fcc3e1ea72f2abff9
Dixon <dixonl2004@gmail.com>
2022-03-02 16:49:52 -0500

    added rest systems

--- pythonProject/ComingAlong.py
+++ pythonProject/ComingAlong.py
@@ -5,8 +5,9 @@ SENTINEL = -1
 
 
 # function clears the console for cleaner use of the program
-def clear():
+def clear(amount):
     os.system('cls')
+    time.sleep(amount)
 
 
 # Start of functions that are in regards to battle scenarios
@@ -176,16 +177,16 @@ def character_skill_learn(skill, character):
 
 
 def player_turn(character, list_enemies):
-    clear()
+    clear(1)
     start_list_length = len(list_enemies)
     print("There are " + str(len(list_enemies)) + " enemies present.")
     print("It is " + character.name + "'s turn.")
     print("1: Basic Attack")
     print("2: Skills")
     action_choice = int(input("Choose which actions to take: "))
-    clear()
+    clear(1)
     if action_choice == 1:
-        clear()
+        clear(1)
         for i in range(len(list_enemies)):
             print(str(i+1) + ": " + list_enemies[i].name + " has " + str(list_enemies[i].health) + " health")
         enemy_selection = int(input("Which enemy do you wish to attack? "))
@@ -194,12 +195,12 @@ def player_turn(character, list_enemies):
         print(character.name + " attacked " + enemy_choice.name)
         new_enemy_health = basic_attack(character, enemy_choice)
         enemy_choice.health = new_enemy_health
-        clear()
+        clear(1)
         if enemy_choice.health > 0:
             print(character.name + " dealt " + str(enemy_start_health - new_enemy_health) + " damage")
             print(enemy_choice.name + " now has " + str(enemy_choice.health) + " health.")
             print("")
-            time.sleep(3)
+            time.sleep(2)
             return list_enemies
         else:
             print(character.name + " dealt " + str(enemy_start_health - new_enemy_health) + " damage")
@@ -210,17 +211,17 @@ def player_turn(character, list_enemies):
             return list_enemies
 
     if action_choice == 2:
-        clear()
+        clear(1)
         for i in range(len(character.skill)):
             print(str(i + 1) + ": " + character.skill[i].name)
         skill_choice = int(input("Which skill do you wish to use? "))
-        clear()
+        clear(1)
         skill_choice = character.skill[skill_choice - 1]
         if skill_choice.enemies == 1:
             for i in range(len(list_enemies)):
                 print(str(i+1) + ": " + list_enemies[i].name + " has " + str(list_enemies[i].health) + " health")
             enemy_selection = int(input("Which enemy do you wish to attack? "))
-            clear()
+            clear(1)
             enemy_choice = list_enemies[enemy_selection - 1]
             start_enemy_health = enemy_choice.health
             damage_output = skill_damage(skill_choice, character, enemy_choice)
@@ -340,7 +341,7 @@ def enemy_turn(enemy, list_characters):
 
 
 def battle_scenario(player_team):
-    clear()
+    clear(1)
     enemy_list = enemies_present(player_main)
     amount_enemies = len(enemy_list)
     turn_count = 1
@@ -378,7 +379,7 @@ def battle_scenario(player_team):
 
 def character_gain_exp(character_list, amount_enemies):
     for i in range(len(character_list)):
-        amount_exp_gained = amount_enemies * character_list[i].level
+        amount_exp_gained = amount_enemies * (character_list[i].level - random.randint(-1, 1))
         print(character_list[i].name + " has gained " + str(amount_exp_gained) + " exp.")
         time.sleep(3)
         character_list[i].exp += amount_exp_gained
@@ -391,7 +392,7 @@ def character_gain_exp(character_list, amount_enemies):
 
 
 def gold_gain(amount_enemies):
-    clear()
+    clear(1)
     initial_gold = item_pack.gold
     for i in range(amount_enemies):
         gold_amount = player_main.level * random.uniform(1.0, 10.0)
@@ -403,9 +404,42 @@ def gold_gain(amount_enemies):
 # End of combat scenario functions
 
 
-# Start of character level-up and stat checking functions
+# Start of character level-up and stat checking functions and generation functions
 # ______________________________________________________________________________________________________________________
 # ______________________________________________________________________________________________________________________
+
+
+def random_stat_generator():
+    value = random.randint(1, 4)
+    if value == 1:
+        return "phys"
+    elif value == 2:
+        return "mag"
+    elif value == 3:
+        return "def"
+    elif value == 4:
+        return "luck"
+
+
+def weapon_generator(num_generated):
+    for i in range(num_generated):
+        stat = random_stat_generator()
+        new_weapon = Weapons(random.randint(1, 10), stat, random.randint(1, 10), "Test Input")
+        item_pack.weapons.append(new_weapon)
+
+
+def accessory_generator(num_generated):
+    for i in range(num_generated):
+        stat = random_stat_generator()
+        coin_flip = random.randint(0, 1)
+        if coin_flip == 0:
+            stat_two = random_stat_generator()
+            stat_two_value = random.randint(0, 10)
+        else:
+            stat_two = "null"
+            stat_two_value = 0
+        new_accessory = Accessory(stat, random.randint(1,10), stat_two, stat_two_value, "Test input")
+        item_pack.accessories.append(new_accessory)
 
 
 # The function first prompts which character has leveled up and then asks the user to input
@@ -416,9 +450,9 @@ def gold_gain(amount_enemies):
 def level_up_assignment(character):
     points = 5
     while points > 0:
-        clear()
+        clear(1)
         print(character.name + " has leveled up!")
-        print("1 = Strength, 2 = Magic, 3 = Defense, 5 = Luck")
+        print("1 = Strength, 2 = Magic, 3 = Defense, 4 = Luck")
         stat = int(input("Which stat would you like to level? "))
         if stat == 1:
             point_reduction = stat_prompt("strength", points)
@@ -450,7 +484,7 @@ def point_prompt(stat, points):
 def stat_prompt(stat, points):
     allocated_points = point_prompt(str(stat), points)
     while allocated_points > points:
-        clear()
+        clear(1)
         print("That is more points then you have")
         allocated_points = point_prompt(str(stat), points)
     return allocated_points
@@ -458,7 +492,7 @@ def stat_prompt(stat, points):
 
 # The function will display the name of the input character and there stats
 def character_stat_list(character):
-    clear()
+    clear(1)
     print(character.name)
     print("Health = " + str(character.health) + "/" + str(character.max_health))
     print("Strength = " + str(character.phys))
@@ -472,7 +506,7 @@ def character_stat_list(character):
 def check_stats():
     checked_character = 0
     while checked_character != SENTINEL:
-        clear()
+        clear(1)
         print("Whose stats would you like to check? ")
         checked_character = int(input("1 for " + player_main.name + ", 2 for " + brawler.name +
                                       ", 3 for " + mage.name + ", and 4 for " + tank.name + ", -1 to return : "))
@@ -491,33 +525,51 @@ def check_stats():
 
 
 def weapon_list(list_choice):
+    clear(1)
     while True:
         for i in range (len(list_choice)):
             print(list_choice[i].name + ": Attack = " + str(list_choice[i].add_dam))
             print("         " + list_choice[i].stat_mod + " : " + str(list_choice[i].stat_mod_num))
-        user_selection = int(input("Enter -1 to return"))
+        user_selection = int(input("Enter -1 to return: "))
+        if user_selection == -1:
+            break
+
+
+def accessory_list(list_choice):
+    clear(1)
+    while True:
+        for i in range(len(list_choice)):
+            print(list_choice[i].name + " : " + str(list_choice[i].b_e_o) + " = " + str(list_choice[i].b_e_o_v), end=" ")
+            if list_choice[i].b_e_t == "null":
+                print(" ")
+                print(" ")
+                pass
+            else:
+                print("and " + list_choice[i].b_e_t + " = " + str(list_choice[i].b_e_t_v))
+                print(" ")
+        user_selection = int(input("Enter -1 to return: "))
         if user_selection == -1:
             break
 
 
 def check_inventory():
     while True:
-        clear()
-        print("You currently have " + str(item_pack.gold) + "gold")
+        clear(1)
+        print("You currently have " + str(item_pack.gold) + " gold")
         print("1: Weapons List ")
         print("2: Accessory List ")
         user_selection = int(input("Chose a list to view, -1 to return: "))
         if user_selection == 1:
             weapon_list(item_pack.weapons)
         elif user_selection == 2:
-            pass
+            accessory_list(item_pack.accessories)
         elif user_selection == -1:
             break
 
 
 def party_inventory_menu():
     while True:
-        clear()
+        clear(1)
         print("1: View the current party ")
         print("2: Edit the party -WIP")
         print("3: View your current inventory ")
@@ -536,14 +588,69 @@ def party_inventory_menu():
 # End of character level-up and stat check functions
 
 
+# Start of functions for town interactions
+# ______________________________________________________________________________________________________________________
+# ______________________________________________________________________________________________________________________
+
+def character_rest(location):
+    while True:
+        if location == "inn":
+            clear(1)
+            print("To sleep in the inn you must pay 10 gold per party member")
+            print("You currently have " + str(item_pack.gold) + " gold.")
+            for i in range(len(player_list)):
+                if i < len(player_list) - 1:
+                    print(player_list[i].name + " has " + str(player_list[i].health) +
+                          "/" + str(player_list[i].max_health) + " health", end=", ")
+                else:
+                    print(player_list[i].name + " has " + str(player_list[i].health) +
+                          "/" + str(player_list[i].max_health) + " health")
+            print("1: Pay " + str(len(player_list*10)) + " gold. ")
+            print("2: Leave the inn")
+            user_choice = int(input("What do you do? "))
+            if user_choice == 1:
+                clear(1)
+                if item_pack.gold - (len(player_list)*10) >= 0:
+                    for i in range(len(player_list)):
+                        player_list[i].health = player_list[i].max_health
+                    print("The party rested and there health was restored. ")
+                    time.sleep(2)
+                    break
+                else:
+                    print("You do not have enough gold to sleep in the inn.")
+                    time.sleep(2)
+                    break
+            elif user_choice == 2:
+                clear(1)
+                print("You leave the inn.")
+                time.sleep(2)
+                break
+
+        elif location == "wild":
+            dice_roll = random.randint(0, 100)
+            if dice_roll > 50:
+                clear(1)
+                print("You successfully slept in the wilderness and the party was healed to full. ")
+                for i in range(len(player_list)):
+                    player_list[i].health = player_list[i].max_health
+                time.sleep(2)
+                break
+            else:
+                clear(1)
+                print("You were attacked while sleeping. ")
+                time.sleep(2)
+                battle_scenario(player_list)
+                break
+
+
 def start_town_menu(team_list):
     while True:
-        clear()
+        clear(1)
         print("You are currently in the town of Dhornisas")
         time.sleep(2)
         print("1: Go to the shop - WIP")
-        print("2: Rest in the local inn -WIP ")
-        print("3: Rest in the wilderness -WIP ")
+        print("2: Rest in the local inn ")
+        print("3: Rest in the wilderness ")
         print("4: Check the local job board -WIP")
         print("5: Go clear the local wilderness of enemies")
         print("6: Manage the party and inventory ")
@@ -551,9 +658,9 @@ def start_town_menu(team_list):
         if user_selection == 1:
             pass
         elif user_selection == 2:
-            pass
+            character_rest("inn")
         elif user_selection == 3:
-            pass
+            character_rest("wild")
         elif user_selection == 4:
             pass
         elif user_selection == 5:
@@ -640,39 +747,6 @@ class Accessory:
         self.name = name
 
 
-def random_stat_generator():
-    value = random.randint(1, 4)
-    if value == 1:
-        return "phys"
-    elif value == 2:
-        return "mag"
-    elif value == 3:
-        return "def"
-    elif value == 4:
-        return "luck"
-
-
-def weapon_generator():
-    for i in range(5):
-        stat = random_stat_generator()
-        new_weapon = Weapons(random.randint(1, 10), stat, random.randint(1, 10), "Test Input")
-        item_pack.weapons.append(new_weapon)
-
-
-def accessory_generator():
-    stat = random_stat_generator()
-    coin_flip = random.randint(0,1)
-    if coin_flip == 0:
-        stat_two = random_stat_generator()
-        stat_two_value = random.randint(0,10)
-    else:
-        stat_two = "null"
-        stat_two_value = 0
-    for i in range(5):
-        new_accessory = Accessory(stat, random.randint(1,10), stat_two, stat_two_value, "Test input")
-    item_pack.weapons.append(new_accessory)
-
-
 P011 = Skill("phys", 0, 1, 1, "Bash")
 P111 = Skill("phys", 1, 1, 1, "Bonk")
 P084 = Skill("phys", 0, 8, 4, "Hassou Tobi")
@@ -696,7 +770,6 @@ enemy_three = BasicEnemy(0, 0, 0, 0, 0, "Enemy Three", 0, [P011, P111], 3)
 enemy_four = BasicEnemy(0, 0, 0, 0, 0, "Enemy Four", 0, [P011, P111], 4)
 
 player_list = [player_main, brawler, mage, tank]
-# player_turn(player_main, enemy_list)
 
-weapon_generator()
+
 start_town_menu(player_list)
